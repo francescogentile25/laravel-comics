b23c9ec5bfb5b4b35e7c7777916174820f0a0544
francescogentile25 <francescogentile250893@gmail.com>
2023-04-05 16:35:19 +0200

    FEAT add  header and footer with @include

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vue Comics</title>
+
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    @include('partials.header')
+
+    <main class="bg-light">
+        {{-- contenuto --}}
+        @yield('content')
+    </main>
+
+    @include('partials.footer')
+
+</body>
+
+</html
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header class="main-header">
+    <div class="container">
+        <img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="">
+        {{-- <img src="/adv.jpg" alt=""> --}}
+        <!-- {{-- <img src="../../img/logo.png" alt=""> questo path relativo non non viene processato in blade--}} -->
+    </div>
+</header>
