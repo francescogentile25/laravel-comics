d02ab0d876258f9829f3966ed743f128554b8408
francescogentile25 <francescogentile250893@gmail.com>
2023-04-05 17:35:46 +0200

    ERROR with img

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,11 +9,10 @@
         <div class="row">
             @foreach ($comics as $comic)
             <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">{{ $comic['title'] }}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
+                    <p class="card-text">{{ $comic['description'] }}</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
